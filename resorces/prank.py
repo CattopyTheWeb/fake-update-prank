22d58d0d3676acde522fa8bf04b299aafd31296d
mes visi draugai <lukavvaiva495@gmail.com>
2022-10-04 23:42:27 +0100

    Initial commit

---
+++ resorces/prank.py
@@ -0,0 +1,33 @@
+from time import sleep
+import webbrowser
+import platform
+import pyautogui
+
+OS = platform.system()
+if OS == "macOS":
+    edge_path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+    webbrowser.register("edge", None, webbrowser.BackgroundBrowser(edge_path))
+
+    webbrowser.get("edge").open("https://updatefaker.com/osx/index.html")
+
+elif OS == "Windows":
+    ver = platform.release()
+    
+    if ver == "10":
+        edge_path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+        webbrowser.register("edge", None, webbrowser.BackgroundBrowser(edge_path))
+
+        webbrowser.get("edge").open("https://updatefaker.com/windows10/index.html")
+        sleep(5)
+        pyautogui.press("f11")
+    
+    elif ver == "11":
+        edge_path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+        webbrowser.register("edge", None, webbrowser.BackgroundBrowser(edge_path))
+
+        webbrowser.get("edge").open("https://updatefaker.com/windows11/index.html")
+        sleep(5)
+        pyautogui.press("f11")
